50b7ea867e6f6a21821eef6cb49f88932383f05f
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-16 16:06:36 +0530

    changes

--- src/components/Tempapp.js
+++ src/components/Tempapp.js
@@ -1,1 +1,30 @@
 import React from 'react';
+import './css/style.css';
+
+const Tempapp = () => {
+    return (
+        <>
+            <div className="box">
+                <div className="inputData">
+                    <input
+                        type="search"
+                        className="inputFeild"
+                        onChange={(event) => {
+
+                        }} />
+                </div>
+            </div>
+            <div className="info">
+                <h2 className="location">
+                    <i class="fas fa-street-view"> Dehradun</i>
+                </h2>
+                <h1 className="temp">
+
+                </h1>
+                <h3 className="tempmin_max">Min : 5.25Cel | Max : 5.25Cel</h3>
+            </div>
+        </>
+    )
+}
+
+export default Tempapp;
