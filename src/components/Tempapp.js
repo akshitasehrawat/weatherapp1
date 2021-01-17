55356d7873197394234251962fa31f31f082826e
sakshidulwani <dulwanisakshi@gmail.com>
2021-01-17 19:20:57 +0530

    changes

--- src/components/Tempapp.js
+++ src/components/Tempapp.js
@@ -4,42 +4,52 @@ import './css/style.css';
 const Tempapp = () => {
 
     const [city, setCity] = useState(null);
-    const [search, setSearch] = useState("Mumbai");
+    const [search, setSearch] = useState("Dehradun");
 
     useEffect(() => {
         const fetchApi = async () => {
-            const url = `api.openweathermap.org/data/2.5/weather?q=${search}&appid=${process.env.WEATHER_API_KEY}`
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=5775f458a287a20b491797af3438d32b`
             const response = await fetch(url);
-            console.log(response)
             const resJson = await response.json();
+            setCity(resJson.main);
         }
 
         fetchApi();
-    })
+    }, [search])
     return (
         <>
             <div className="box">
                 <div className="inputData">
                     <input
                         type="search"
+                        value={search}
                         className="inputFeild"
                         onChange={(event) => {
-
+                            setSearch(event.target.value)
                         }} />
                 </div>
+                {!city ? (
+                    <p className="errMsg"> No Data Found</p>
+                ) : (
+                        <div>
+                            <div className="info">
+                                <h2 className="location">
+                                    <i className="fas fa-street-view " style={{ color: "white" }}></i>
+                                    {search}
 
-                <div className="info">
-                    <h2 className="location">
-                        <i className="fas fa-street-view " style={{ color: "white" }}></i>Dehradun
-                    </h2>
-                    <h1 className="temp">
-                        5.25Cel
-                </h1>
-                    <h3 className="tempmin_max">Min : 5.25Cel | Max : 5.25Cel</h3>
-                </div>
-                <div className="wave"></div>
-                <div className="wave-two"></div>
-                <div className="wave-three"></div>
+                                </h2>
+                                <h1 className="temp">
+                                    {city.temp}°Cel
+                                </h1>
+                                <h3 className="tempmin_max">{city.temp_min}°Cel | {city.temp_max}°Cel</h3>
+                            </div>
+                            <div className="wave"></div>
+                            <div className="wave-two"></div>
+                            <div className="wave-three"></div>
+                        </div>
+                    )
+                }
+
             </div>
         </>
     )
